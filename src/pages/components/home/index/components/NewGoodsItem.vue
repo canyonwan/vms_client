e234f592e3d9d82f3985b882cb7006dcbb078f1f
canyonwan <canyonwan@126.com>
2023-07-27 22:22:51 +0800

    feat: 新增新品横向滑动view

---
+++ src/pages/components/home/index/components/NewGoodsItem.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="new-goods-item tn-pr-lg tn-radius">
+    <tn-lazy-load
+      width="250" height="250" src="https://file.canyonwan.top/57012f01b7a2ddd5.jpg" mode="widthFix"
+    />
+    <div class="tn-mt-sm">
+      <tn-title title="阿维菌素透皮溶液阿维菌素透皮溶液" size="lg" />
+    </div>
+    <view class="tn-red_text tn-flex tn-flex-col-center items-end">
+      <!-- <view class="logo-pic ">
+              <view class="logo-image">
+                <view class="tn-round" :style="`background-image:url(${item.userImage});width: 40rpx;height: 40rpx;background-size: cover;`" />
+              </view>
+            </view> -->
+      <view>
+        <text class="tn-text-xs">
+          ¥
+        </text>
+      </view>
+      <view>
+        <text class="tn-text-xl tn-text-bold">
+          899
+        </text>
+      </view>
+    </view>
+  </div>
+</template>
