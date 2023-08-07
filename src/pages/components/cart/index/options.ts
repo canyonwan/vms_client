90bab1ce5e160ccb379300d55693c78a79629b73
canyonwan <canyonwan@126.com>
2023-08-07 11:45:21 +0800

    feat: 新增购物车swipe配置

---
+++ src/pages/components/cart/index/options.ts
@@ -0,0 +1,11 @@
+import type { SwipeActionItemOption } from '@tuniao/tnui-vue3-uniapp'
+
+export const swipeOptions: SwipeActionItemOption = [
+  {
+    text: '删除',
+    icon: 'delete',
+    bgColor: 'tn-red',
+    disabled: true,
+    round: true,
+  },
+]
