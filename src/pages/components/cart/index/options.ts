f1fb44eee152a29eecec413941bee1a30df66d70
canyonwan <canyonwan@126.com>
2023-08-13 23:00:25 +0800

    feat: 新增通用配置

--- src/pages/components/cart/index/options.ts
+++ src/pages/components/cart/index/options.ts
@@ -1,11 +1,18 @@
-import type { SwipeActionItemOption } from '@tuniao/tnui-vue3-uniapp'
+import type { LoadmoreText, SwipeActionItemOption } from '@tuniao/tnui-vue3-uniapp'
 
 export const swipeOptions: SwipeActionItemOption = [
   {
     text: '删除',
     icon: 'delete',
     bgColor: 'tn-red',
-    disabled: true,
+    // disabled: true,
     round: true,
   },
 ]
+
+export const customLoadMoreText: LoadmoreText = {
+  loadmore: '还有内容呢',
+  loading: '等等，数据正在赶来',
+  nomore: '哎呀，没有啦',
+  empty: '空空的呀',
+}
