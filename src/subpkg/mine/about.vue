85d3aea3347ffcb196c0bb05d6c4fdeadee79007
canyonwan <canyonwan@126.com>
2023-08-16 22:47:25 +0800

    feat: 新增关于我们页面

---
+++ src/subpkg/mine/about.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view class="about">
+    <tn-navbar fixed home-icon="" bottom-shadow bg-color="#8397C8">
+      关于我们
+    </tn-navbar>
+    <view>一个商城小程序</view>
+  </view>
+</template>
+
+<style scoped lang="scss">
+// .about{
+
+// }
+</style>
